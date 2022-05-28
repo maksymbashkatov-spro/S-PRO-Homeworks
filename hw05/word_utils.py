6694523c9d8f987ece81d1e1391e137dbedc1e53
Максим Башкатов <maksymbashkatov.spro@gmail.com>
2022-05-28 17:43:44 +0300

    Was fixed hw05_task2.

---
+++ hw05/word_utils.py
@@ -0,0 +1,11 @@
+def str_without_punctuation(user_str):
+    return ''.join(i for i in user_str if i.isalpha() or i == ' ')
+
+
+def get_list_of_words(user_str):
+    return str_without_punctuation(user_str).split()
+
+
+def get_more_long_word(user_str):
+    t_l1 = get_list_of_words(user_str)
+    return max([i.lower() for i in t_l1], key=len)
